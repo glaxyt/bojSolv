5453ba4cdc614f8e60c12fd49e70379b25369d34
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2022-05-06 11:31:24 +0900

    Create 15904.py

---
+++ 15904.py
@@ -0,0 +1,14 @@
+# 15904번 UCPC는 무엇의 약자일까?
+word = input()
+check = True
+
+for i in "UCPC":
+    if i in word:
+        word = word[word.index(i)+1:]
+    else:
+        check = False
+        break
+
+if check: print("I love UCPC")
+else: print("I hate UCPC")
+        
