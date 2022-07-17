cd942a42eb61ed478c370b194fdc551e3d3c20fc
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2022-07-18 03:31:38 +0900

    Add files via upload

--- 10866.py
+++ 10866.py
@@ -1,9 +1,41 @@
-# 10886번 0 = not cute / 1 = cute
-t = int(input())
-yes, no = 0, 0
-for _ in range(t):
-    n = input()
-    if n == '1': yes += 1
-    else: no += 1
-if yes > no: print("Junhee is cute!")
-else: print("Junhee is not cute!")
+# 10866번 덱
+from collections import deque
+import sys
+
+n = int(input())
+queue = deque()
+for _ in range(n):
+    li = sys.stdin.readline().rstrip().split()
+    order = li[0]
+    if order == "push_front":
+        queue.appendleft(int(li[1]))
+    elif order == "push_back":
+        queue.append(int(li[1]))
+    elif order == "pop_front":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif order == "pop_back":
+        if queue:
+            print(queue.pop())
+        else:
+            print(-1)
+    elif order == "size":
+        print(len(queue))
+
+    elif order == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif order == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    else:
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
