b66ef234086609a7352ed12a7420032b0da46381
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2023-08-09 05:25:27 +0900

    백준 2470

---
+++ 2470.py
@@ -0,0 +1,21 @@
+# 2470번 두 용액
+import sys
+
+n = int(input())
+liquid = list(map(int, input().split()))
+liquid.sort()
+start = 0
+end = n-1
+closeToEnd = sys.maxsize 
+ans = [0,0]
+while start < end:
+    total = liquid[start] + liquid[end]
+    if abs(total) < closeToEnd:
+        closeToEnd = abs(total)
+        ans[0], ans[1] = liquid[start], liquid[end]
+    elif total > 0:
+        end -=1
+    else:
+        start += 1
+
+print(*ans)
