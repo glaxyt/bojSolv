ed60e49e8593f4d3369222b11d7ee1513d334e88
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2023-08-14 12:00:32 +0900

    백준 14921

---
+++ 14921.py
@@ -0,0 +1,29 @@
+# 14921번 용액 합성하기
+# 투포인터와 완전탐색 사용.
+import sys
+n = int(input())
+liquid = list(map(int, input().split()))
+min_sumOfLiquid = sys.maxsize
+ans = 0
+
+if n == 2:
+        print(sum(liquid))
+else:
+    start, end = 0, n-1
+    while start < end:
+            sumOfLiquid = liquid[start] + liquid[end]
+
+            if abs(sumOfLiquid) < min_sumOfLiquid:
+                min_sumOfLiquid = abs(sumOfLiquid)
+                ans = sumOfLiquid
+                
+            if sumOfLiquid > 0:
+                end -= 1
+            else:
+                start += 1
+
+            if sumOfLiquid == 0:
+                ans = 0
+                break
+            
+    print(ans)
