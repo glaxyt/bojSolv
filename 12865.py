bb48230aef8cf9b19e3543591951441faf99d83d
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2023-08-24 12:06:56 +0900

    백준 12865

---
+++ 12865.py
@@ -0,0 +1,13 @@
+# 12865번 평범한 배낭
+import sys
+n, k = map(int, input().split())
+dp = [[0] * (k+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    w, v = map(int, sys.stdin.readline().split())
+    for j in range(1, k+1):
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+print(dp[n][k])
