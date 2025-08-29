0d045b35e36bbd24cb625b32286984f0b2b235d7
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2025-08-29 11:11:53 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,44 +1,53 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
+// 10:30 시작
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int h = input[0];
         int w = input[1];
+        // 빗물이 고이는 장소를 탐색하는 것.
+        // 스택인가? 그리디로 풀 수 있을듯.
+        // 첫 땅의 높이를 저장. 이후 이것보다 높은게 나온다면 새롭게 갱신
+        // 첫땅의 높이를 저장. 이후 이것보다 낮은게 나온다면 일단 계산 시작. 첫땅 높이 - 낮은땅
+        // 첫땅의 높이를 저장 이후 이것과 같은게 나온다면 새롭게 갱신
+        // 낮은땅으로 갱신하다가. -> 높은땅이 나온다 혹은 나랑 같은 땅이 나온다. -> 합산
 
-        int[] grid = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[] lands = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int leftLandHeight = lands[0];
+        int leftIdx = 0;
+        int idx = 1;
         int answer = 0;
-        int left_idx = 0;
-        int right_idx = w - 1;
-
-        for (int i = 1; i < w; i++) {
-            if (grid[left_idx] <= grid[i]) {
-                if (grid[left_idx] == 0) {
-                    left_idx = i;
-                    continue;
-                }
-
-                for (int j = left_idx; j < i; j++) {
-                    answer += grid[left_idx] - grid[j];
-                }
-
-                left_idx = i;
-
-            } else {
-                if (i == w - 1) {
-                    for (int j = w - 1; j > left_idx; j--) {
-                        if (grid[right_idx] > grid[j]) {
-                            answer += grid[right_idx] - grid[j];
-                        } else {
-                            right_idx = j;
-                        }
+        while (idx < w) {
+            // 첫 땅의 높이보다 같거나 높을 경우에는 갱신.
+            if (idx == w - 1 && leftLandHeight > lands[idx]) {
+                int rightLandHeight = lands[idx];
+                for (int i = w - 2; i > leftIdx; i--) {
+                    if (rightLandHeight < lands[i]) {
+                        rightLandHeight = lands[i];
+                    } else {
+                        answer += rightLandHeight - lands[i];
                     }
                 }
+                break;
             }
+            // 3 0 3 1 2 1
+            // 3 2 1 2 1 2
+            // 6 5 4 3 2 1
+
+            if (leftLandHeight <= lands[idx]) {
+                int standard = Math.min(leftLandHeight, lands[idx]);
+                for (int i = leftIdx; i < idx; i++) {
+                    answer += standard - lands[i];
+                }
+                leftLandHeight = lands[idx];
+                leftIdx = idx;
+            }
+            idx++;
         }
+
         System.out.println(answer);
     }
 }
